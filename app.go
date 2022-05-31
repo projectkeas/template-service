00c0be3fbc876b92c6072da13ca4fab010e9206a
Stuart Blackler (@im5tu) <im5tu@users.noreply.github.com>
2022-05-31 17:48:49 +0100

    Updated entrypoint

--- app.go
+++ app.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/projectkeas/sdks-service/configuration"
 	"github.com/projectkeas/sdks-service/server"
 )
 
@@ -16,12 +15,12 @@ func main() {
 	app.WithConfigMap("config-1")
 	app.WithSecret("secret-1")
 
-	app.ConfigureHandlers(func(f *fiber.App, configurationAccessor func() *configuration.ConfigurationRoot) {
+	app.ConfigureHandlers(func(f *fiber.App, server *server.Server) {
 		f.Get("/", func(c *fiber.Ctx) error {
-			value := configurationAccessor().GetStringValueOrDefault("log.level", "not set")
+			value := server.GetConfiguration().GetStringValueOrDefault("log.level", "not set")
 			return c.SendString(fmt.Sprintf("Hello, World 👋! Log Level is: %s", value))
 		})
 	})
 
-	app.Run()
+	app.Build().Run()
 }
